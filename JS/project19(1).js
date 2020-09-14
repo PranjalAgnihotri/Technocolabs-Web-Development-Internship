a206251eba0a86aa1387c8f4ac6b5c5e0be30bdf
PranjalAgnihotri <70158300+PranjalAgnihotri@users.noreply.github.com>
2020-09-14 21:43:30 +0530

    Add files via upload

---
+++ JS/project19(1).js
@@ -0,0 +1,20 @@
+var ind = require('./index');
+import {Index,payGrade, getCadre, calculateTax, getBenefits,     calculateBonus , reimbursementEligibility } from './index.js';
+
+function getEmployeeInformation(inputSalary) {
+  console.log(inputSalary);
+  Index.salary = inputSalary;
+  var a='Cadre: ';
+  var b='Tax: ';
+  var c='Benefits: ';
+  var d='Bonus: ';
+  var e='Reimbursement Eligibility: ';
+  return (a + getCadre());
+  return ( b+ calculateTax());
+  return (c+ getBenefits());
+  return (d + calculateBonus());
+  return ( e+  reimbursementEligibility());
+}
+getEmployeeInformation(10000);
+getEmployeeInformation(50000);
+getEmployeeInformation(100000);
